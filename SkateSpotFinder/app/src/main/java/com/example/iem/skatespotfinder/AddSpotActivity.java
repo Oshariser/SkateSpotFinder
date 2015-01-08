a040f0365a0c405c4a13e5d29dd3bd34634386fa
BaptisteBR <baptiste.briot@gmail.com>
2015-01-08 12:06:12 +0100

    Commit getLocation

--- SkateSpotFinder/app/src/main/java/com/example/iem/skatespotfinder/AddSpotActivity.java
+++ SkateSpotFinder/app/src/main/java/com/example/iem/skatespotfinder/AddSpotActivity.java
@@ -1,5 +1,8 @@
 package com.example.iem.skatespotfinder;
 
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,10 +13,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
 
 
 public class AddSpotActivity extends ActionBarActivity {
+    private LatLng mLatLng;
 
+    private Button mButtonLocalisation;
+    private TextView mTextViewLatitude;
+    private TextView mTextViewLongitude;
     private ImageView mImageViewSpot;
     private RatingBar mRatingBarSpot;
     private EditText mEditTextDescription;
@@ -24,6 +37,33 @@ public class AddSpotActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_spot);
 
+        mButtonLocalisation = (Button) findViewById(R.id.buttonLocalisation);
+        mButtonLocalisation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocationManager mLocationManager=null;
+                LocationListener mLocationListener;
+                mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                mLocationListener = new MyLocationListener();
+                mLocationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+
+                if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    if(MyLocationListener.mLatitude>0)
+                    {
+                        mTextViewLatitude.append("Latitude:- " + MyLocationListener.mLatitude + '\n');
+                        mTextViewLongitude.append("Longitude:- " + MyLocationListener.mLongitude + '\n');
+                    }
+                    else
+                    {
+                        Toast.makeText(AddSpotActivity.this, "GPS in progress, please wait.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(AddSpotActivity.this, "GPS is not turned on...", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        mTextViewLatitude = (TextView) findViewById(R.id.textViewLatitude);
+        mTextViewLongitude = (TextView) findViewById(R.id.textViewLongitude);
         mImageViewSpot = (ImageView)findViewById(R.id.imageViewSpot);
         mRatingBarSpot = (RatingBar)findViewById(R.id.ratingBarSpot);
         mEditTextDescription = (EditText)findViewById(R.id.editTextDescription);
@@ -31,7 +71,7 @@ public class AddSpotActivity extends ActionBarActivity {
         mButtonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Spot lSpot = new Spot();
             }
         });
     }
@@ -58,4 +98,12 @@ public class AddSpotActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void addSpot(Spot aSpot){
+        ParseObject lParseObject = new ParseObject("Spot");
+        lParseObject.put("localisation", new ParseGeoPoint(aSpot.getLatitude(), aSpot.getLongitude()));
+        lParseObject.put("image", aSpot.getImage());
+        lParseObject.put("rating", aSpot.getRating());
+        lParseObject.put("description", aSpot.getDescription());
+        lParseObject.saveInBackground();
+    }
 }
--- SkateSpotFinder/app/src/main/java/com/example/iem/skatespotfinder/Spot.java
+++ SkateSpotFinder/app/src/main/java/com/example/iem/skatespotfinder/Spot.java
@@ -1,18 +1,60 @@
 package com.example.iem.skatespotfinder;
 
-import android.media.Image;
-
 /**
  * Created by root on 1/8/15.
  */
 public class Spot {
+    private double mLatitude;
+    private double mLongitude;
     private byte[] mImage;
     private int mRating;
     private String mDescription;
 
-    public Spot(byte[] aImage, int aRating, String aDescription) {
-        this.mImage = aImage;
-        this.mRating = aRating;
-        this.mDescription = aDescription;
+    public Spot(double aLatitude, double aLongitude, byte[] aImage, int aRating, String aDescription) {
+        this.setLatitude(aLatitude);
+        this.setLongitude(aLongitude);
+        this.setImage(aImage);
+        this.setRating(aRating);
+        this.setDescription(aDescription);
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(double mLongitude) {
+        this.mLongitude = mLongitude;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(double mLatitude) {
+        this.mLatitude = mLatitude;
+    }
+
+    public byte[] getImage() {
+        return mImage;
+    }
+
+    public void setImage(byte[] mImage) {
+        this.mImage = mImage;
+    }
+
+    public int getRating() {
+        return mRating;
+    }
+
+    public void setRating(int mRating) {
+        this.mRating = mRating;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String mDescription) {
+        this.mDescription = mDescription;
     }
 }
